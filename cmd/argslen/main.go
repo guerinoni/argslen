c86baf59c617f14bd556595ec652e07468b3639b
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-09-06 23:30:15 +0200

    Add -v or --version command line args
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- cmd/argslen/main.go
+++ cmd/argslen/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/guerinoni/argslen/pkg/analyzer"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
+var version string
+
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		fmt.Fprintf(os.Stdout, "argslen version: %s\n", version)
+
+		os.Exit(0)
+	}
+
 	singlechecker.Main(analyzer.NewAnalyzer())
 }
